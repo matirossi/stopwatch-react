c70580c8c42b3b99438ea10e668518219309f67e
Mati Rossi <mati.rossi@weareadaptive.com>
2022-10-06 11:56:55 +0200

    delete unused FormattedTime, log behavior inside run timer

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,32 @@ function App() {
   const [elapsedTime, setElapsedTime] = useState(0)
   const [isTimeRunning, setIsTimeRunning] = useState()
   const [startingTime, setStartingTime] = useState(0)
-  const [timeAtPause, setTimeAtPause] = useState(0)
-  const [formattedTime, setFormattedTime] = useState(updateTimer(0))
+  //const [timeAtPause, setTimeAtPause] = useState(0)
   const [animationFrameId, setAnimationFrameId] = useState(0)
 
   const runTimer = (timestamp) => {
-    if (startingTime === 0) setStartingTime(timestamp)
+   // if (startingTime === 0) setStartingTime(timestamp)//not inmediatly available!
+    //console.log(startingTime) //why is always 0 and not the previous value? 
 
-    setElapsedTime(timestamp - startingTime)
+    setElapsedTime(prev => {
+      console.log("previous elapsed time",prev)
+      console.log("timestamp", timestamp)
+      console.log("startingTime", startingTime);
+      return timestamp - startingTime})
     setAnimationFrameId(requestAnimationFrame(runTimer))
   }
-
-  useEffect(() => {
-    setFormattedTime(updateTimer(elapsedTime))
-  }, [elapsedTime])
 
   useEffect(() => {
     if (isTimeRunning) {
       setAnimationFrameId(requestAnimationFrame(runTimer))
     }
-    return () => { cancelAnimationFrame(animationFrameId) } //if the component is uunmounted while the time is runnning
-  }, [isTimeRunning]);
-
-  useEffect(() => {
     if (!isTimeRunning) {
       cancelAnimationFrame(animationFrameId)
       setStartingTime(0)
-      setTimeAtPause(elapsedTime)
+  //    setTimeAtPause(elapsedTime)
     }
-  }, [isTimeRunning])
+    return () => { cancelAnimationFrame(animationFrameId) } //if the component is uunmounted while the time is runnning
+  }, [isTimeRunning]);
 
   const handleClick = () => {
     setIsTimeRunning(!isTimeRunning)
@@ -44,7 +41,7 @@ function App() {
   return (
     <div className="App">
       <div className="main-timer-container">
-        <span className="main-timer">{formattedTime}</span>
+        <span className="main-timer">{updateTimer(elapsedTime)}</span>
         <div className="buttons-container">
           {!isTimeRunning && <button className="start-button" onClick={handleClick}>start</button>}
           {isTimeRunning && <button className="stop-button" onClick={handleClick}>stop</button>}
